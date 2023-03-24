b92f861bdc62c3d099beea3a477d914ca49318d2
Dennis Diatlov <dennis.dyatlov@gmail.com>
2023-03-24 17:34:58 +0000

    Refactor action handling

--- tamagotchi/src/lib.rs
+++ tamagotchi/src/lib.rs
@@ -1,13 +1,14 @@
 #![no_std]
 
-use gstd::{msg, prelude::*, ActorId};
+use crate::tamagotchi::Tamagotchi;
+use gstd::{future::Future, msg, pin::Pin, prelude::*, ActorId};
+use tamagotchi_io::{TmgAction, TmgReply};
 
 static mut TAMAGOTCHI: Option<tamagotchi::Tamagotchi> = None;
 static mut FTOKEN_CONTRACT_ID: Option<ActorId> = None;
-static mut APPROVE_SPENDING_TX_MANAGER: Option<tx_manager::TxManager<tamagotchi_io::TmgAction>> =
-    None;
+static mut APPROVE_SPENDING_TX_MANAGER: Option<tx_manager::TxManager<(ActorId, u128)>> = None;
 static mut PURCHASE_ATTRIBUTE_TX_MANAGER: Option<
-    tx_manager::TxManager<tamagotchi_io::TmgAction, tamagotchi_io::AttributeId>,
+    tx_manager::TxManager<(ActorId, u32), tamagotchi_io::AttributeId>,
 > = None;
 
 #[no_mangle]
@@ -30,184 +31,8 @@ async fn main() {
     let action: tamagotchi_io::TmgAction = msg::load().expect("Unable to read action message");
     let tamagotchi = unsafe { TAMAGOTCHI.as_mut().expect("Tamagotchi is not initialized") };
     ensure_action_is_authorized(actor_id, &action, tamagotchi);
-    match action {
-        tamagotchi_io::TmgAction::Name => {
-            msg::reply(tamagotchi_io::TmgReply::Name(tamagotchi.name().into()), 0)
-                .expect("Unable to reply with tamagotchi name");
-        }
-        tamagotchi_io::TmgAction::Age => {
-            msg::reply(tamagotchi_io::TmgReply::Age(tamagotchi.age()), 0)
-                .expect("Unable to reply with tamagotchi age");
-        }
-        tamagotchi_io::TmgAction::Feed => {
-            tamagotchi.feed();
-            msg::reply(tamagotchi_io::TmgReply::Fed, 0)
-                .expect("Unable to reply with tamagotchi fed event");
-        }
-        tamagotchi_io::TmgAction::Play => {
-            tamagotchi.play();
-            msg::reply(tamagotchi_io::TmgReply::Entertained, 0)
-                .expect("Unable to reply with tamagotchi entertained event");
-        }
-        tamagotchi_io::TmgAction::Sleep => {
-            tamagotchi.sleep();
-            msg::reply(tamagotchi_io::TmgReply::Slept, 0)
-                .expect("Unable to reply with tamagotchi slept event");
-        }
-        tamagotchi_io::TmgAction::TransferOwnership(new_owner_id) => {
-            tamagotchi.transfer_to(new_owner_id);
-            msg::reply(
-                tamagotchi_io::TmgReply::OwnershipTransferred(new_owner_id),
-                0,
-            )
-            .expect("Unable to reply with tamagotchi transferred event");
-        }
-        tamagotchi_io::TmgAction::GrantOwnershipTransfer(transferor_id) => {
-            tamagotchi.grant_transfer_permission(transferor_id);
-            msg::reply(
-                tamagotchi_io::TmgReply::OwnershipTransferGranted(transferor_id),
-                0,
-            )
-            .expect("Unable to reply with tamagotchi transfer permission granted event");
-        }
-        tamagotchi_io::TmgAction::RevokeOwnershipTransfer => {
-            tamagotchi.revoke_transfer_permission();
-            msg::reply(tamagotchi_io::TmgReply::OwnershipTransferRevoked, 0)
-                .expect("Unable to reply with tamagotchi transfer permission revoked event");
-        }
-        tamagotchi_io::TmgAction::SetFTokenContract(ftoken_contract_id) => {
-            unsafe {
-                FTOKEN_CONTRACT_ID = Some(ftoken_contract_id);
-            }
-            msg::reply(tamagotchi_io::TmgReply::FTokenContractSet, 0)
-                .expect("Unable to reply with ftoken contract set");
-        }
-        tamagotchi_io::TmgAction::ApproveSpending {
-            spendor_id: store_contract_id,
-            amount,
-        } => {
-            let ftoken_contract_id = unsafe {
-                FTOKEN_CONTRACT_ID
-                    .as_ref()
-                    .expect("FToken contract is not initialized")
-            };
-            let tx_manager = unsafe {
-                APPROVE_SPENDING_TX_MANAGER
-                    .as_mut()
-                    .expect("Approve spending tx manager is not initialized")
-            };
-
-            let open_tx_result = tx_manager.open_tx(*ftoken_contract_id, action.clone(), (), false); // This is persisted in the first UoW.
-            if let Err(open_tx_error) = open_tx_result {
-                msg::reply(
-                    tamagotchi_io::TmgReply::ApproveSpendingError(open_tx_error.into()),
-                    0,
-                )
-                .expect("Unable to reply with ApproveSpendingError");
-                return;
-            }
-
-            let call_result = msg::send_for_reply(
-                *ftoken_contract_id,
-                ft_main_io::FTokenAction::Message {
-                    transaction_id: open_tx_result.unwrap(),
-                    payload: ft_logic_io::Action::Approve {
-                        approved_account: store_contract_id,
-                        amount,
-                    }
-                    .encode(), // <- The interface lacks of being strongly typed - not good.
-                },
-                0,
-            )
-            .expect("Unable to send ApproveSpending")
-            .await; // <-- The `send_for_reply` call splits the whole method into 2 UoWs which is not good/obvious to my POV.
-
-            tx_manager
-                .close_tx(*ftoken_contract_id, action, open_tx_result.unwrap()) // This is persisted in the second UoW.
-                .unwrap_or_default(); // We swallow out of order or transaction closing.
-            if let Err(contract_error) = call_result {
-                msg::reply(
-                    tamagotchi_io::TmgReply::ApproveSpendingError(format!("{:?}", contract_error)),
-                    0,
-                )
-                .expect("Unable to reply with ApproveSpendingError");
-                return;
-            }
-
-            msg::reply(
-                tamagotchi_io::TmgReply::SpendingApproved {
-                    spendor_id: store_contract_id,
-                    amount,
-                },
-                0,
-            )
-            .expect("Unable to reply with SpendingApproved");
-        }
-        tamagotchi_io::TmgAction::PurchaseAttribute {
-            store_id: store_contract_id,
-            attribute_id,
-        } => {
-            let tx_manager = unsafe {
-                PURCHASE_ATTRIBUTE_TX_MANAGER
-                    .as_mut()
-                    .expect("Purchase attribute tx manager is not initialized")
-            };
-
-            let open_tx_result =
-                tx_manager.open_tx(store_contract_id, action.clone(), attribute_id, true);
-            if open_tx_result.is_err() {
-                msg::reply(
-                    tamagotchi_io::TmgReply::PurchaseAttributeInProgressError(
-                        *tx_manager.pending_tx_context(),
-                    ),
-                    0,
-                )
-                .expect("Unable to reply with PurchaseAttributeInProgressError");
-                return;
-            }
-
-            let call_result = msg::send_for_reply(
-                store_contract_id,
-                store_io::StoreAction::BuyAttribute { attribute_id },
-                0,
-            )
-            .expect("Unable to send PurchaseAttribute")
-            .await;
-
-            tx_manager
-                .close_tx(store_contract_id, action, open_tx_result.unwrap())
-                .unwrap_or_default();
-            if let Err(contract_error) = call_result {
-                msg::reply(
-                    tamagotchi_io::TmgReply::PurchaseAttributeError(format!(
-                        "{:?}",
-                        contract_error
-                    )),
-                    0,
-                )
-                .expect("Unable to reply with PurchaseError");
-                return;
-            }
-
-            msg::reply(tamagotchi_io::TmgReply::AttributePurchased(attribute_id), 0)
-                .expect("Unable to reply with AttributePurchased");
-        }
-        tamagotchi_io::TmgAction::Transfer(actor_id) => {
-            tamagotchi.transfer(msg::source(), actor_id);
-            msg::reply(tamagotchi_io::TmgEvent::Transfer(actor_id), 0)
-                .expect("Unable to reply with tamagochi transferred event");
-        }
-        tamagotchi_io::TmgAction::Approve(transferor_actor_id) => {
-            tamagotchi.grant_transfer_permission(msg::source(), transferor_actor_id);
-            msg::reply(tamagotchi_io::TmgEvent::Approve(transferor_actor_id), 0)
-                .expect("Unable to reply with tamagochi transfer permission granted event");
-        }
-        tamagotchi_io::TmgAction::RevokeApproval => {
-            tamagotchi.revoke_transfer_permission(msg::source());
-            msg::reply(tamagotchi_io::TmgEvent::RevokeApproval, 0)
-                .expect("Unable to reply with tamagochi transfer permission revoked event");
-        }
-    }
+    let action_handler = match_handler(action);
+    action_handler(tamagotchi).await;
 }
 
 #[no_mangle]
@@ -223,10 +48,195 @@ extern "C" fn metahash() {
     msg::reply(metahash, 0).expect("Unable to reply with metahash");
 }
 
+fn match_handler<'a>(
+    action: TmgAction,
+) -> Box<dyn FnOnce(&'a mut Tamagotchi) -> Pin<Box<dyn Future<Output = ()> + 'a>>> {
+    match action {
+        TmgAction::Name => Box::new(|tmg: &mut Tamagotchi| Box::pin(name_handler(tmg))),
+        TmgAction::Age => Box::new(|tmg: &mut Tamagotchi| Box::pin(age_handler(tmg))),
+        TmgAction::Feed => Box::new(|tmg| Box::pin(feed_handler(tmg))),
+        TmgAction::Play => Box::new(|tmg| Box::pin(play_handler(tmg))),
+        TmgAction::Sleep => Box::new(|tmg| Box::pin(sleep_handler(tmg))),
+        TmgAction::TransferOwnership(new_owner_id) => {
+            Box::new(move |tmg| Box::pin(transfer_ownership_handler(tmg, new_owner_id)))
+        }
+        TmgAction::GrantOwnershipTransfer(transferror_id) => {
+            Box::new(move |tmg| Box::pin(grant_ownership_transfer_handler(tmg, transferror_id)))
+        }
+        TmgAction::RevokeOwnershipTransfer => {
+            Box::new(|tmg| Box::pin(revoke_ownership_transfer(tmg)))
+        }
+        TmgAction::SetFTokenContract(ftoken_contract_id) => {
+            Box::new(move |_| Box::pin(set_ftoken_contract_handler(ftoken_contract_id)))
+        }
+        TmgAction::ApproveSpending { spendor_id, amount } => {
+            Box::new(move |_| Box::pin(approve_spending_handler(spendor_id, amount)))
+        }
+        TmgAction::PurchaseAttribute {
+            store_id: store_contract_id,
+            attribute_id,
+        } => {
+            Box::new(move |_| Box::pin(purchase_attribute_handler(store_contract_id, attribute_id)))
+        }
+    }
+}
+
+async fn name_handler(tamagotchi: &Tamagotchi) {
+    msg::reply(TmgReply::Name(tamagotchi.name().into()), 0).expect("Unable to reply with Name");
+}
+
+async fn age_handler(tamagotchi: &Tamagotchi) {
+    msg::reply(TmgReply::Age(tamagotchi.age()), 0).expect("Unable to reply with Age");
+}
+
+async fn feed_handler(tamagotchi: &mut Tamagotchi) {
+    tamagotchi.feed();
+    msg::reply(TmgReply::Fed, 0).expect("Unable to reply with Fed");
+}
+
+async fn play_handler(tamagotchi: &mut Tamagotchi) {
+    tamagotchi.play();
+    msg::reply(TmgReply::Entertained, 0).expect("Unable to reply with Entertained");
+}
+
+async fn sleep_handler(tamagotchi: &mut Tamagotchi) {
+    tamagotchi.sleep();
+    msg::reply(TmgReply::Slept, 0).expect("Unable to reply with Slept");
+}
+
+async fn transfer_ownership_handler(tamagotchi: &mut Tamagotchi, new_owner_id: ActorId) {
+    tamagotchi.transfer_to(new_owner_id);
+    msg::reply(TmgReply::OwnershipTransferred(new_owner_id), 0)
+        .expect("Unable to reply with OwnershipTransferred");
+}
+
+async fn grant_ownership_transfer_handler(tamagotchi: &mut Tamagotchi, transferror_id: ActorId) {
+    tamagotchi.grant_transfer_permission(transferror_id);
+    msg::reply(TmgReply::OwnershipTransferGranted(transferror_id), 0)
+        .expect("Unable to reply with OwnershipTransferGranted");
+}
+
+async fn revoke_ownership_transfer(tamagotchi: &mut Tamagotchi) {
+    tamagotchi.revoke_transfer_permission();
+    msg::reply(TmgReply::OwnershipTransferRevoked, 0)
+        .expect("Unable to reply with OwnershipTransferRevoked");
+}
+
+async fn set_ftoken_contract_handler(ftoken_contract_id: ActorId) {
+    unsafe {
+        FTOKEN_CONTRACT_ID = Some(ftoken_contract_id);
+    }
+    msg::reply(TmgReply::FTokenContractSet, 0).expect("Unable to reply with FTokenContractSet");
+}
+
+async fn approve_spending_handler(spendor_id: ActorId, amount: u128) {
+    let ftoken_contract_id = unsafe {
+        FTOKEN_CONTRACT_ID
+            .as_ref()
+            .expect("FToken contract is not initialized")
+    };
+    let tx_manager = unsafe {
+        APPROVE_SPENDING_TX_MANAGER
+            .as_mut()
+            .expect("Approve spending tx manager is not initialized")
+    };
+    let tx_data = (spendor_id, amount);
+
+    let open_tx_result = tx_manager.open_tx(*ftoken_contract_id, tx_data, (), false); // This is persisted in the first UoW.
+    if let Err(open_tx_error) = open_tx_result {
+        msg::reply(
+            tamagotchi_io::TmgReply::ApproveSpendingError(open_tx_error.into()),
+            0,
+        )
+        .expect("Unable to reply with ApproveSpendingError");
+        return;
+    }
+
+    let call_result = msg::send_for_reply(
+        *ftoken_contract_id,
+        ft_main_io::FTokenAction::Message {
+            transaction_id: open_tx_result.unwrap(),
+            payload: ft_logic_io::Action::Approve {
+                approved_account: spendor_id,
+                amount,
+            }
+            .encode(), // <- The interface lacks of being strongly typed - not good.
+        },
+        0,
+    )
+    .expect("Unable to send ApproveSpending")
+    .await; // <-- The `send_for_reply` call splits the whole method into 2 UoWs which is not good/obvious to my POV.
+
+    tx_manager
+        .close_tx(*ftoken_contract_id, tx_data, open_tx_result.unwrap()) // This is persisted in the second UoW.
+        .unwrap_or_default(); // We swallow out of order or transaction closing.
+    if let Err(contract_error) = call_result {
+        msg::reply(
+            tamagotchi_io::TmgReply::ApproveSpendingError(format!("{:?}", contract_error)),
+            0,
+        )
+        .expect("Unable to reply with ApproveSpendingError");
+        return;
+    }
+
+    msg::reply(
+        tamagotchi_io::TmgReply::SpendingApproved { spendor_id, amount },
+        0,
+    )
+    .expect("Unable to reply with SpendingApproved");
+}
+
+async fn purchase_attribute_handler(
+    store_contract_id: ActorId,
+    attribute_id: tamagotchi_io::AttributeId,
+) {
+    let tx_manager = unsafe {
+        PURCHASE_ATTRIBUTE_TX_MANAGER
+            .as_mut()
+            .expect("Purchase attribute tx manager is not initialized")
+    };
+    let tx_data = (store_contract_id, attribute_id);
+
+    let open_tx_result = tx_manager.open_tx(store_contract_id, tx_data, attribute_id, true);
+    if open_tx_result.is_err() {
+        msg::reply(
+            tamagotchi_io::TmgReply::PurchaseAttributeInProgressError(
+                *tx_manager.pending_tx_context(),
+            ),
+            0,
+        )
+        .expect("Unable to reply with PurchaseAttributeInProgressError");
+        return;
+    }
+
+    let call_result = msg::send_for_reply(
+        store_contract_id,
+        store_io::StoreAction::BuyAttribute { attribute_id },
+        0,
+    )
+    .expect("Unable to send PurchaseAttribute")
+    .await;
+
+    tx_manager
+        .close_tx(store_contract_id, tx_data, open_tx_result.unwrap())
+        .unwrap_or_default();
+    if let Err(contract_error) = call_result {
+        msg::reply(
+            tamagotchi_io::TmgReply::PurchaseAttributeError(format!("{:?}", contract_error)),
+            0,
+        )
+        .expect("Unable to reply with PurchaseError");
+        return;
+    }
+
+    msg::reply(tamagotchi_io::TmgReply::AttributePurchased(attribute_id), 0)
+        .expect("Unable to reply with AttributePurchased");
+}
+
 fn ensure_action_is_authorized(
     actor_id: ActorId,
     action: &tamagotchi_io::TmgAction,
-    tamagotchi: &tamagotchi::Tamagotchi,
+    tamagotchi: &Tamagotchi,
 ) {
     match action {
         tamagotchi_io::TmgAction::Name | tamagotchi_io::TmgAction::Age => {}
